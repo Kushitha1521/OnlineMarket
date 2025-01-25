3335f07245fc734a554aeea63a7534f082d1556d
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2025-01-25 16:10:34 +0530

    update

--- my-backend/index.js
+++ my-backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const products = [
-  { id: 1, name: 'Laptop', price: '$999', image: 'https://images.unsplash.com/photo-1612838818789-8e9689e59b08' },
+  { id: 1, name: 'Laptop and', price: '$999', image: 'https://images.unsplash.com/photo-1612838818789-8e9689e59b08' },
   { id: 2, name: 'Smartphone', price: '$499', image: 'https://images.unsplash.com/photo-1604079112971-8b34f07b3952' },
   { id: 3, name: 'Headphones', price: '$199', image: 'https://images.unsplash.com/photo-1612223428664-423e2404ebdd' },
   { id: 4, name: 'Smartwatch', price: '$129', image: 'https://images.unsplash.com/photo-1593642634441-87b6c56f20a7' },
